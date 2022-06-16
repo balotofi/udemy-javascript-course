418f73f779df63de63bb58bab1b6978579066062
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-16 22:15:07 +0100

    completed section 1 assignment and added section 2 source code

--- section-1/script.js
+++ section-1/script.js
@@ -1,1 +1,6 @@
-alert('hello 3');
+// write to documment
+document.write('This is my welcome message');
+// popup message
+alert("hello");
+//console message
+console.log("this is the console message")
